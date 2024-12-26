df20e41d717b1c1a1c0c73592ca45b050c24e4d0
Apfirebolt <aspper20@gmail.com>
2024-12-26 14:42:19 +0530

    fix: changes in allowed hosts

--- django_movie_api/settings.py
+++ django_movie_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 't55x2*!cyr@(wu80sc=85mg#p$$z*5r)#ypourwe2ga&$3c+p#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['206.81.29.244', 'localhost', 'http://softgenie.org']
+ALLOWED_HOSTS = ['206.81.29.244', 'localhost', 'http://softgenie.org', 'softgenie.org', 'www.softgenie.org']
 
 
 # Application definition
