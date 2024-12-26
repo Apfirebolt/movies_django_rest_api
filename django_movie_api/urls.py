6d2a96fb3b4214655042876d692d502a9290b4de
Apfirebolt <aspper20@gmail.com>
2024-12-26 15:05:03 +0530

    templates and static files added

--- django_movie_api/urls.py
+++ django_movie_api/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include(('api.urls', 'api'), namespace='api')),
     path('api-schema/', SpectacularAPIView.as_view(), name='schema'),
