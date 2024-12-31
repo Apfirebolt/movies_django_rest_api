9024a76e686a6d7c495026dca13913b9d5d9ab04
Apfirebolt <aspper20@gmail.com>
2024-12-31 18:36:12 +0530

    added templates, forms and libraries

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django_movie_api import settings
-from . views import home_view, LoginView, RegisterUser
+from . views import LoginView, RegisterUser
 import django.contrib.auth.views as AuthViews
 
 
 urlpatterns = [
-    path('', home_view, name='home'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', AuthViews.LogoutView.as_view(), name='logout'),
     path('register', RegisterUser.as_view(), name='register'),
--- blog/views.py
+++ blog/views.py
@@ -7,46 +7,46 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class BlogDetailView(DetailView):
     model = Blog
     context_object_name = 'blog'
-    template_name = 'blog/detail_blog.html'
+    template_name = 'blog/blog-detail.html'
 
 
 class BlogListView(ListView):
     model = Blog
-    template_name = 'blog/list_blog.html'
+    template_name = 'blog/blog-list.html'
     context_object_name = 'blogs'
 
 
 class ProjectDetailView(DetailView):
     model = Project
     context_object_name = 'project'
-    template_name = 'project/detail_project.html'
+    template_name = 'projects/project-detail.html'
 
 
 class ProjectListView(ListView):
     model = Project
-    template_name = 'project/list_project.html'
+    template_name = 'projects/project-list.html'
     context_object_name = 'projects'
 
 
 class GalleryPostDetailView(DetailView):
     model = GalleryPost
     context_object_name = 'gallery_post'
-    template_name = 'gallery/detail_gallery_post.html'
+    template_name = 'gallery/gallery-detail.html'
 
 
 class GalleryPostListView(ListView):
     model = GalleryPost
-    template_name = 'gallery/list_gallery_post.html'
+    template_name = 'gallery/gallery-list.html'
     context_object_name = 'gallery_posts'
 
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
     context_object_name = 'blog_post'
-    template_name = 'blog/detail_blog_post.html'
+    template_name = 'blog/post-detail.html'
 
 
 class BlogPostListView(ListView):
     model = BlogPost
-    template_name = 'blog/list_blog_post.html'
+    template_name = 'blog/post-list.html'
     context_object_name = 'blog_posts'
